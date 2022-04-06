816e368d9e7b02768a8b988d74ba994d7d6c7e79
javaMercy <nextelonmusk01@gmail.com>
2022-04-07 02:18:08 +0300

    Finally Done

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.getElementById("form");
+const input_el = document.getElementById("email");
+
+form.addEventListener("submit", (e) => {
+
+    const email = input_el.value.trim();
+    if (!email) {
+
+        showAlert("danger", "Plase enter an email!");
+    } else {
+
+        if (ValidateEmail(email)) {
+
+
+            showAlert("success", "Successfully done!");
+        } else {
+
+            showAlert("danger", "Email format is unvalid.");
+
+
+        }
+
+    }
+    e.preventDefault();
+})
+
+
+function ValidateEmail(email) {
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return true;
+    }
+
+    return false;
+}
+
+function showAlert(type, message) {
+
+    const alert = document.createElement("div");
+    alert.className = "alert "+ type; 
+    alert.textContent = message;
+
+    form.appendChild(alert);
+
+    setTimeout(() => {
+
+        alert.remove();
+    }, 1500)
+
+}
