b067082344073b26c4743161e14bb18537314c16
javaMercy <nextelonmusk01@gmail.com>
2022-04-07 13:15:09 +0300

    final touchings

--- script.js
+++ script.js
@@ -2,49 +2,36 @@ const form = document.getElementById("form");
 const input_el = document.getElementById("email");
 
 form.addEventListener("submit", (e) => {
-
-    const email = input_el.value.trim();
-    if (!email) {
-
-        showAlert("danger", "Plase enter an email!");
+  const email = input_el.value.trim();
+  if (!email) {
+    showAlert("danger", "Plase enter an email!");
+  } else {
+    if (ValidateEmail(email)) {
+      showAlert("success", "Successfully done!");
+      input_el.value="";
     } else {
-
-        if (ValidateEmail(email)) {
-
-
-            showAlert("success", "Successfully done!");
-        } else {
-
-            showAlert("danger", "Email format is unvalid.");
-
-
-        }
-
+      showAlert("danger", "Email format is unvalid.");
     }
-    e.preventDefault();
-})
-
+  }
+  e.preventDefault();
+});
 
 function ValidateEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return true;
+  }
 
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-        return true;
-    }
-
-    return false;
+  return false;
 }
 
 function showAlert(type, message) {
+  const alert = document.createElement("div");
+  alert.className = "alert " + type;
+  alert.textContent = message;
 
-    const alert = document.createElement("div");
-    alert.className = "alert "+ type; 
-    alert.textContent = message;
+  form.appendChild(alert);
 
-    form.appendChild(alert);
-
-    setTimeout(() => {
-
-        alert.remove();
-    }, 1500)
-
+  setTimeout(() => {
+    alert.remove();
+  }, 1500);
 }
